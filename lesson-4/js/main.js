27d468134e02d2c32dcc7d1fc76698a02d9bf932
Clayton <clayton.fortini@gmail.com>
2019-10-30 13:45:21 -0400

    Lesson 4 - Task 3

--- lesson-4/js/main.js
+++ lesson-4/js/main.js
@@ -7,3 +7,16 @@
 * */
 
 console.log("This outputs into the console.");
+
+let toggleButton = docum.createElement("button");
+
+let a = document.getElementsByTagName("a")[0];
+
+a.setAttribute("title", "Fortini Games");
+
+let strengthsList = document.getElementById("strengths").getElementsByTagName("li");
+strengthsList[1].setAttribute("class", "new-class");
+
+let newDiv = document.createElement("div");
+let main = document.getElementsByClassName("main")[0];
+main.appendChild(newDiv);
