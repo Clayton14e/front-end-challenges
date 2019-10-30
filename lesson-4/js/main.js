8afe1661e413f7b3cb0b65d64dcfe7468e5bae66
Clayton <clayton.fortini@gmail.com>
2019-10-30 15:39:00 -0400

    Lesson 4 - Toggle Button

--- lesson-4/js/main.js
+++ lesson-4/js/main.js
@@ -5,18 +5,45 @@
 *
 * Notes: Lorem ipsum justo vel
 * */
+// Toggle Function
+// let toggleFunction = function(){
+//     var x = document.getElementById("new-div");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
 
 console.log("This outputs into the console.");
 
-let toggleButton = docum.createElement("button");
+let newDiv = document.createElement("div");
+let main = document.getElementsByClassName("main")[0];
+newDiv.id = "new-div";
+main.appendChild(newDiv);
+newDiv.innerHTML = "the New"
+
+// let runFunction = function(){
+let toggleButton = document.createElement("button");
+toggleButton.innerHTML = "Toggle";
+toggleButton.onclick = function(){
+    var x = document.getElementById("new-div");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+document.body.appendChild(toggleButton);
 
 let a = document.getElementsByTagName("a")[0];
-
 a.setAttribute("title", "Fortini Games");
+// a.innerHTML = "New New";
 
 let strengthsList = document.getElementById("strengths").getElementsByTagName("li");
 strengthsList[1].setAttribute("class", "new-class");
 
-let newDiv = document.createElement("div");
-let main = document.getElementsByClassName("main")[0];
-main.appendChild(newDiv);
+
+// }
+
+
